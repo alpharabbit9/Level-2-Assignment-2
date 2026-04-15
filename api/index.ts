107d28764a506643061ee7373b124dc32ebd9b01
alpharabbit9 <alpharabbit74@gmail.com>
2026-04-16 00:33:11 +0600

    chore: ensure db init in app for vercel

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,3 @@
 import app from '../src/app';
-import { initDb } from '../src/config/db';
 
-let isDbInitialized = false;
-
-export default async (req: any, res: any) => {
-  if (!isDbInitialized) {
-    await initDb();
-    isDbInitialized = true;
-  }
-  
-  return app(req, res);
-};
+export default app;
