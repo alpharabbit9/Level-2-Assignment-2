b43a3634f988ce0df313fc0cda82e966bff988d3
alpharabbit9 <alpharabbit74@gmail.com>
2026-04-16 00:52:45 +0600

    fix: switch to bcryptjs and explicit vercel builder

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { query } from '../../config/db';
 import { AppError } from '../../utils/AppError';
